98ad82a81ab80391ab374fff4877de4f52fea8d1
Matteo Codogno <matteo.codogno@welld.ch>
2021-08-10 17:15:54 +0200

    chore: improve solution for "scrabble score" exercise

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,27 +1,35 @@
 package scrabble
 
-import "strings"
+import (
+	"unicode"
+)
+
+func letterScore(letter rune) int {
+	switch unicode.ToUpper(letter) {
+	case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+		return 1
+	case 'D', 'G':
+		return 2
+	case 'B', 'C', 'M', 'P':
+		return 3
+	case 'F', 'H', 'V', 'W', 'Y':
+		return 4
+	case 'K':
+		return 5
+	case 'J', 'X':
+		return 8
+	case 'Q', 'Z':
+		return 10
+	default:
+		return 0
+	}
+}
 
 func Score(word string) int {
 	score := 0
 
-	for i := range word {
-		switch strings.ToUpper(string(word[i])) {
-		case "A", "E", "I", "O", "U", "L", "N", "R", "S", "T":
-			score += 1
-		case "D", "G":
-			score += 2
-		case "B", "C", "M", "P":
-			score += 3
-		case "F", "H", "V", "W", "Y":
-			score += 4
-		case "K":
-			score += 5
-		case "J", "X":
-			score += 8
-		case "Q", "Z":
-			score += 10
-		}
+	for _, c := range word {
+		score += letterScore(c)
 	}
 
 	return score
